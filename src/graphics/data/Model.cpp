0c1b6014c3f71a4d816d5898be3c30f63d0b6b2a
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2020-10-13 23:23:44 +0200

    Add a model class
    This is to help with loading and binding of the different buffers

---
+++ src/graphics/data/Model.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Tobias on 10/13/2020.
+//
+
+#include <glad/glad.h>
+
+#include <utility>
+
+#include "Model.h"
+
+Model Model::from_data(const DataLayout &dataLayout, const float *data, GLuint amount) {
+	GLuint vao, vbo;
+	// Generate a Vertex Array Object (for now without data)
+	glGenVertexArrays(1, &vao);
+	// Generate a Vertex Buffer object (also without data)
+	glGenBuffers(1, &vbo);
+	// Bind the VAO so upcoming changes are saved here
+	glBindVertexArray(vao);
+
+	// Bind the VBO to fill it with data
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	// TODO different modes apart from GL_STATIC_DRAW
+	// Fill the VBO with data
+	glBufferData(GL_ARRAY_BUFFER, amount, data, GL_STATIC_DRAW);
+
+	// Bind the DataLayout (basically multiple glVertexAttribPointers enabled)
+	dataLayout.bind();
+
+	// properly unbind all buffers, arrays, etc. to prevent collisions
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// TODO unbind dataLayout as well somehow
+
+	return Model(vao, amount);
+}
+
+Model::Model(GLuint vao, GLuint vertexCount) : m_vao{vao}, m_vertexCount{vertexCount} {}
+
+void Model::draw() const {
+	glBindVertexArray(m_vao);
+	glDrawArrays(GL_TRIANGLES, 0, m_vertexCount);
+	glBindVertexArray(0);
+}
---
+++ src/graphics/data/Model.h
@@ -0,0 +1,40 @@
+//
+// Created by Tobias on 10/13/2020.
+//
+
+#ifndef SD3D_MODEL_H
+#define SD3D_MODEL_H
+
+#include <GLFW/glfw3.h>
+#include "DataLayout.h"
+
+class Model {
+	// TODO data structure for data layout
+private:
+	// Vertex Array Object
+	// - This object holds info about data layout, the VBO the data is coming from
+	//   and the linked EBO (if applicable)
+	GLuint m_vao;
+	GLuint m_vertexCount;
+
+	// The VBO is implicitly saved in the VAO
+
+	// TODO support EBO later
+//	// Element Buffer Object
+//	// - This object doesn't have to be bound
+//	// - If it is bound, it provides a way to reuse vertices multiple times by
+//	//   specifying the indices into the VBO that are used
+//	GLuint m_ebo;
+
+	explicit Model(GLuint vao, GLuint vertexCount);
+
+public:
+	void draw() const;
+
+	// for now only float
+	static Model from_data(const DataLayout &dataLayout, const float *data, GLuint amount);
+
+};
+
+
+#endif //SD3D_MODEL_H
