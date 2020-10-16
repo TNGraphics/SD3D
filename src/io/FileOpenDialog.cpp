85ecfee0bc7abdffd5627137bebcc992627742a9
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2020-10-17 01:40:35 +0200

    A way to open a windows file dialogue
    It should not be used, because it uses VERY old Windows API functionality

---
+++ src/io/FileOpenDialog.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Tobias on 10/14/2020.
+//
+
+#include <windows.h>
+
+#include <spdlog/spdlog.h>
+
+#include "FileOpenDialog.h"
+
+static constexpr const char *g_filterStr = "obj files (*.obj)\0*.obj\0fbx files (*.fbx)\0*.fbx";
+
+namespace sd3d::io {
+
+std::string request_file_dialog(const char *filter) {
+	const auto *f{(!filter ? g_filterStr : filter)};
+	OPENFILENAME ofn;
+	char fileName[MAX_PATH] = "";
+	ZeroMemory(&ofn, sizeof(ofn));
+
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.hwndOwner = nullptr;
+	ofn.lpstrFilter = f;
+	ofn.lpstrFile = fileName;
+	ofn.nMaxFile = MAX_PATH;
+	ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST;
+	ofn.lpstrDefExt = "";
+	if(GetOpenFileName(&ofn)) {
+		return fileName;
+	} else {
+		return "";
+	}
+}
+}
---
+++ src/io/FileOpenDialog.h
@@ -0,0 +1,20 @@
+//
+// Created by Tobias on 10/14/2020.
+//
+
+#ifndef SD3D_FILEOPENDIALOG_H
+#define SD3D_FILEOPENDIALOG_H
+
+#include <string>
+
+// TODO also look at https://github.com/AirGuanZ/imgui-filebrowser
+// TODO also look at https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/bb776913(v=vs.85)
+
+namespace sd3d::io {
+
+[[deprecated("This uses a very old WinApi")]]
+std::string request_file_dialog(const char *filter = nullptr);
+
+}
+
+#endif //SD3D_FILEOPENDIALOG_H
