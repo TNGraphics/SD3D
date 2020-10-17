f3ef952fc13cc32b24ea592b34a574b438032051
Tobias Nienhaus <tobiasnienhaus69@gmail.com>
2020-10-17 19:48:09 +0200

    Minor formatting, remove deprecated stuff

--- src/main.cpp
+++ src/main.cpp
@@ -40,8 +40,6 @@
 #include "controls/GeneralInputHandler.h"
 #include "controls/OrbitCameraController.h"
 
-void framebuffer_size_callback(GLFWwindow *, int width, int height);
-
 void fps_gui();
 
 float g_mixVal{};
@@ -79,21 +77,13 @@ int main(int argc, const char *argv[]) {
 
 	stbi_set_flip_vertically_on_load(true);
 
-	GlContext glContext{GlContext::Settings{.width = width,
-											.height = height}};
+	GlContext glContext{GlContext::Settings{.width = width, .height = height}};
 	if (!glContext) {
 		spdlog::error("Error setting up GL Context!");
 		return -1;
 	}
 
 	gui::setup_imgui(glContext.win());
-
-	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
-		glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
-		glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
-		glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
 
 	GeneralInputHandler inputHandler{glContext.win()};
 
